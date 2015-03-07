4c5f89e284af030b248c3b59c6866ec980cd97f8
sus0 <susie_su94@hotmail.com>
2015-03-07 15:18:26 -0500

    Add wrapper class for controller

---
+++ Unity/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControl : MonoBehaviour {
+
+	[HideInInspector]
+	public WarriorControllerBase playerCtrl;
+	// Use this for initialization
+	void Start () {
+		playerCtrl = new DefaultWarriorController();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
