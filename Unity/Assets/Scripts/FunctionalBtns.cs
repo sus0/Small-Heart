21901942e6051b7c7ae9f8b208e8bef2bd63ecef
sus0 <susie_su94@hotmail.com>
2015-03-08 09:02:04 -0400

    Main Menu done.

---
+++ Unity/Assets/Scripts/FunctionalBtns.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FunctionalBtns : MonoBehaviour {
+	public void StartBtnOnClick()
+	{
+		Application.LoadLevel("MainScene");
+	}
+	public void ContinueBtnOnClick()
+	{
+		Debug.Log("Continue button not implemented yet.");
+		Application.LoadLevel("MainScene");
+	}
+	public void ExitBtnOnClick()
+	{
+		Application.Quit();
+	}
+}
