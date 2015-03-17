3e1945ef2dc4bebf353941edd9efdf940adb1df9
sus0 <susie_su94@hotmail.com>
2015-03-16 22:31:32 -0400

    Gui clock done

---
+++ Unity/Assets/Scripts/Time.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System;
+
+public class Time : MonoBehaviour {
+
+	public Text timeTxt;
+
+	private string _txt; 
+	// Update is called once per frame
+	void Update () {
+		_txt = String.Format("{0:00}:{1:00}", DateTime.Now.TimeOfDay.Hours, DateTime.Now.TimeOfDay.Minutes);
+		timeTxt.text = _txt;
+		//Debug.Log(_txt);
+	}
+}
