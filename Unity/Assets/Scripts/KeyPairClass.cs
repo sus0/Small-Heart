bea275dd40d096094a19fd89b94004310ad3ddb9
sus0 <susie_su94@hotmail.com>
2015-03-18 23:25:23 -0400

    Move Character info class out (Preparing to serialize)

--- Unity/Assets/Scripts/KeyPairClass.cs
+++ Unity/Assets/Scripts/KeyPairClass.cs
@@ -60,34 +60,5 @@ namespace SmallHeart
 		}
 	}
 
-	public class Info {
-		public string SpritePath 	 { get; set; }
-		public int 	  CurrStage  	 { get; set; }
-		public string CharacterClass { get; set; }
-		public string ClassInfo      { get; set; }
-		public int    TrainingTime 	 { get; set; }
-		public int    HealthTotal    { get; set; }
-
-		public Info()
-		{}
-
-		public Info (string path, int currStage)
-		{
-			SpritePath     = path;
-			CurrStage  	   = currStage;
-			CharacterClass = "";
-			ClassInfo	   = "";
-		}
-		public bool SetStats (int training, int healthTotal)
-		{
-			TrainingTime = training;
-			HealthTotal  = healthTotal;
-			return true;
-		}
-
-
-	}
-
-
 }
 
