84c47e494dc8e03056232c29c6f00195248a2658
sus0 <susie_su94@hotmail.com>
2015-03-20 23:36:27 -0400

    Split details from info

---
+++ Unity/Assets/Scripts/CharacterDetailsClass.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Xml;
+using System.Xml.Serialization;
+using System.Collections.Generic;
+using System.IO;
+
+
+namespace SmallHeart
+{
+	public class CharacterDetails{
+		// Use this as a key
+		[XmlAttribute("path")]
+		public string SpritePath 	 { get; set; }
+		
+		// Set from xml
+		[XmlAttribute("classInfo")]
+		public string ClassInfo      { get; set; }
+		[XmlAttribute("className")]
+		public string CharacterClass { get; set; }
+	}
+
+	[XmlRoot("InfoCollection")]
+	public class CharacterDetailsContainer
+	{
+		[XmlArray("Route0aInfos")]
+		[XmlArrayItem("Info")]
+		public List<CharacterDetails> Route0aDetails = new List<CharacterDetails>();
+		
+		[XmlArray("Route0bInfos")]
+		[XmlArrayItem("Info")]
+		public List<CharacterDetails> Route0bDetails = new List<CharacterDetails>();
+		
+		[XmlArray("Route1aInfos")]
+		[XmlArrayItem("Info")]
+		public List<CharacterDetails> Route1aDetails = new List<CharacterDetails>();
+		
+		[XmlArray("Route1bInfos")]
+		[XmlArrayItem("Info")]
+		public List<CharacterDetails> Route1bDetails = new List<CharacterDetails>();
+		
+		[XmlArray("Route2aInfos")]
+		[XmlArrayItem("Info")]
+		public List<CharacterDetails> Route2aDetails = new List<CharacterDetails>();
+		
+		[XmlArray("Route2bInfos")]
+		[XmlArrayItem("Info")]
+		public List<CharacterDetails> Route2bDetails = new List<CharacterDetails>();
+		
+		
+		public static CharacterDetailsContainer Load(string xmlPath)
+		{
+			var    serializer = new XmlSerializer(typeof(CharacterDetailsContainer));
+			using( var stream = new FileStream(xmlPath, FileMode.Open) )
+			{
+				return serializer.Deserialize(stream) as CharacterDetailsContainer;
+			}
+		}
+	}
+
+}
--- Unity/Assets/Scripts/CharacterInfoClass.cs
+++ Unity/Assets/Scripts/CharacterInfoClass.cs
@@ -1,40 +1,24 @@
 ﻿using UnityEngine;
 using System.Collections;
-using System.Xml;
-using System.Xml.Serialization;
-using System.Collections.Generic;
-using System.IO;
+
 
 namespace SmallHeart
 {
 	public class Info {
-
-		// Use this as a key
-		[XmlAttribute("path")]
-		public string SpritePath 	 { get; set; }
-
-		// Set from xml
-		[XmlAttribute("classInfo")]
-		public string ClassInfo      { get; set; }
-		[XmlAttribute("className")]
-		public string CharacterClass { get; set; }
-
+		public CharacterDetails details = new CharacterDetails();
 
 		// Set from constructing the dictionary 
-		public int 	  CurrStage  	 { get; set; }
-		public int    TrainingTime 	 { get; set; }
-		public int    HealthTotal    { get; set; }
+		public int 	  CurrStage  	 	{ get; set; }
+		public int    TrainingTime 	 	{ get; set; }
+		public int    HealthTotal    	{ get; set; }
 
 
 		public Info()
 		{}
 		
-		public Info (string path, int currStage)
+		public Info (int currStage)
 		{
-			SpritePath     = path;
 			CurrStage  	   = currStage;
-			CharacterClass = "";
-			ClassInfo	   = "";
 		}
 		public bool SetStats (int training, int healthTotal)
 		{
@@ -43,44 +27,5 @@ namespace SmallHeart
 			return true;
 		}
 	}
-
-	[XmlRoot("InfoCollection")]
-	public class InfoContainer
-	{
-		[XmlArray("Route0aInfos")]
-		[XmlArrayItem("Info")]
-		public List<Info> Route0aInfos = new List<Info>();
-
-		[XmlArray("Route0bInfos")]
-		[XmlArrayItem("Info")]
-		public List<Info> Route0bInfos = new List<Info>();
-
-		[XmlArray("Route1aInfos")]
-		[XmlArrayItem("Info")]
-		public List<Info> Route1aInfos = new List<Info>();
-		
-		[XmlArray("Route1bInfos")]
-		[XmlArrayItem("Info")]
-		public List<Info> Route1bInfos = new List<Info>();
-
-		[XmlArray("Route2aInfos")]
-		[XmlArrayItem("Info")]
-		public List<Info> Route2aInfos = new List<Info>();
-		
-		[XmlArray("Route2bInfos")]
-		[XmlArrayItem("Info")]
-		public List<Info> Route2bInfos = new List<Info>();
-
-
-		public static InfoContainer Load(string xmlPath)
-		{
-			var    serializer = new XmlSerializer(typeof(InfoContainer));
-			using( var stream = new FileStream(xmlPath, FileMode.Open) )
-			{
-				return serializer.Deserialize(stream) as InfoContainer;
-			}
-		}
-	}
-	
 
 }
